6543b78f5f3320e1dcf674a4d834dbc3a47b11d7
leticiap-almeida <144292426+leticiap-almeida@users.noreply.github.com>
2024-08-27 21:36:15 -0300

    Create Atividade_2.js

---
+++ Atividade_2.js
@@ -0,0 +1,25 @@
+Atividade 2
+
+Exemplo 1:
+const quadrado = num => num * num 
+console.log(quadrado(4)); 
+
+Resposta: 16
+
+Exemplo 2:
+ const soma = (a, b) => a + b;
+ console.log(soma(10,5));
+
+Resposta: 15
+
+Exemplo 3:
+const saudacao = () => "Olá, mundo!";
+console.log(saudacao());
+
+Resposta: Olá, mundo!
+
+Exemplo 4:
+const multiplicarPorDois = num => num * 2;
+console.log(multiplicarPorDois(7));
+
+Resposta: 14
